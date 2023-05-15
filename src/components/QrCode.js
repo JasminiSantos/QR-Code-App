7f54b1dc5a8874ab5124443c0c91eb3808bef714
jasminisantos <jasminirebeca10@gmail.com>
2023-05-15 13:18:09 -0300

    QR Code generator development

---
+++ src/components/QrCode.js
@@ -0,0 +1,80 @@
+// src/components/QrCode.js
+
+import { useState, useRef} from "react";
+import { QRCodeCanvas } from "qrcode.react";
+import logo from '../images/Logo-Desktop.svg';
+
+function isValidURL(url) {
+  var pattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+  return pattern.test(url);
+}
+
+function checkURLExists(url) {
+  return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      return false;
+    });
+}
+
+const QrCode = () => {
+  const [url, setUrl] = useState("");
+  const qrRef = useRef();
+
+  const handleDownloadQRCode = (e) => {
+    e.preventDefault();
+    let canvas = qrRef.current.querySelector("canvas");
+    let image = canvas.toDataURL("image/png");
+    let anchor = document.createElement("a");
+    anchor.href = image;
+    anchor.download = `qr-code.png`;
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+    setUrl("");
+  };
+
+  const handleQRCodeEncoder = (e) => {
+    setUrl(e.target.value);
+  };
+
+  const qrcode = (
+    <QRCodeCanvas
+      id="qrCode"
+      value={url}
+      size={300}
+      bgColor={"#ffffff"}
+      level="H"
+    />
+  );
+
+  return (
+    <div className="qrcode__container">
+       <div className="qrcode" ref={qrRef}>{qrcode}</div>
+      <div className="input__group">
+        <img className="logo" src={logo} alt="Logo" />
+        <form onSubmit={handleDownloadQRCode}>
+          <input
+            id="urlInput"
+            type="text"
+            value={url}
+            onChange={handleQRCodeEncoder}
+            placeholder="https://jasminisantos.netlify.app/"
+          />
+          <button type="submit" disabled={!url}>
+            Download QR code
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default QrCode;
