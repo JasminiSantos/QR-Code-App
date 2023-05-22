0e48405d33cac087b8aa45aacceb76a826fa8f2b
jasminisantos <jasminirebeca10@gmail.com>
2023-05-21 21:32:51 -0300

    moving some states to the api provider

--- src/ContextAPI/APIProvider.js
+++ src/ContextAPI/APIProvider.js
@@ -3,10 +3,15 @@ import ApiContext from './APIContext';
 
 const ApiProvider = ({ children }) => {
   const [apiData, setApiData] = useState(null);
+
+  // Propertires and functions that handle the error modal
   const [isOpenModal, setIsOpenModal] = useState(false);
 
+  // Properties and functions that handle the button click of scanner button
+  const [isScannerVisible, setIsScannerVisible] = useState(false);
+  
   return (
-    <ApiContext.Provider value={{ apiData, setApiData, isOpenModal, setIsOpenModal }}>
+    <ApiContext.Provider value={{ apiData, setApiData, isOpenModal, setIsOpenModal, isScannerVisible, setIsScannerVisible }}>
       {children}
     </ApiContext.Provider>
   );
